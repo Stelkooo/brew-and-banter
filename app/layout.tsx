f5c7ca727011dadb3a78e341496b8fe0b5eaf62b
stelko <stilyanmtoskov@gmail.com>
2023-11-14 23:19:47 +0000

    shadcn ui setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,16 @@
 import './globals.css';
+import { Lora, Raleway } from 'next/font/google';
+
+import { cn } from '@/lib/utils';
+
+// Serif Font Import
+const lora = Lora({ subsets: ['latin'], variable: '--font-lora' });
+// Sans Serif Font Import
+const raleway = Raleway({
+  weight: '400',
+  subsets: ['latin'],
+  variable: '--font-raleway',
+});
 
 export default function RootLayout({
   children,
@@ -6,8 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={cn(lora.variable, raleway.variable)}>{children}</body>
     </html>
   );
 }
